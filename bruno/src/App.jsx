a1a21c54a71288f03c3b3dafb45946e5b809665d
Bruno Amadi <brunoamadi8@gmail.com>
2026-07-10 17:12:15 +0100

    #1 Setted up a Footer and created draft design

--- bruno/src/App.jsx
+++ bruno/src/App.jsx
@@ -3,21 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import AppFooter from './AppFooter.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <section className="titleArea">
-        <h1><em>Hello Title</em></h1>
+      <section className="titlearea">
+        <h1 className="website-title">Bruno Amadi</h1>
+        <p className="website-subtitle" >Future Software Developer</p>
       </section>
-      <section className="main">
-
+      <section className="story">
+        <div className="person-photo">
+          <img src="src/assets/dices.jpg" width="700" height="500"/>
+        </div>
+        <div className="person-description">
+          <span>Hello, my name is Bruno Amadi. I am currently 20 year's old and I am aiming to perfect my crafts in software development.</span>
+        </div>
       </section>
-      <footer>
-
-      </footer>
+      <section className="projects">
+      </section>
+      <AppFooter />
     </>
   )
 }
---
+++ bruno/src/AppFooter.jsx
@@ -0,0 +1,41 @@
+import { Bold, Mail, MapPin, Phone } from 'lucide-react';
+
+function AppFooter() {
+    return (
+        <footer>
+            <div className="footer-main-grid">
+                <div className="footer-brand">
+                    <span>Bruno Amadi</span>
+                </div>
+                <div className="footer-contacts">
+                    <h4>Contact Information</h4>
+                    <div className="footer-contact-links">
+                        {[
+                            { icon: <Mail size={16} stroke="black" />, text: "businessemail@hotmail.com" },
+                            { icon: <Phone size={16} stroke="black" />, text: "(+44)1234567890" },
+                        ].map(({ icon, text }, i) => {
+                            return (
+                                <div key={i} className="footer-contact-link">
+                                    <span className="footer-contact-link-icon">{icon}</span>
+                                    <span className="footer-contact-link-name">{text}</span>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+                <div className="footer-feedback">
+                    <h4>Feedback</h4>
+                    <form onSubmit={e => e.preventDefault()} className="footer-feedback-form">
+                        <input type="email" placeholder="Your email address" className="footer-feedback-form-cards" />
+                        <textarea placeholder="Share your thoughts..." className="footer-feedback-form-cards"/>
+                        <button type="submit" className="footer-feedback-form-submit">
+                            Send
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default AppFooter;
