4c3631f7b5e85ce530930a577765fde5b50e0963
Bruhno2006 <brunoamadi8@gmail.com>
2026-07-24 21:29:33 +0100

    #19 Further developed education section

--- bruno/src/components/EducationTable.jsx
+++ bruno/src/components/EducationTable.jsx
@@ -1,19 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { schools } from "../data/educationData.json";
+import "./style/EducationTable.css"
 
 function EducationTable() {
+  const [content, setContent] = useState(0);
+  const [counter, setCounter] = useState(0);
+
+  const totalContents = schools.length;
+
+  const nextContent = () => {
+    setContent(content === totalContents - 1 ? 0 : content + 1);
+  }
+
+  const getVisibility = (idx) => {
+    if (content === idx) {
+      return "content-active"
+    }
+
+    return "content-hidden";
+  }
+
+  const getContainerVisibility = (state) => {
+    if (state === "content-active") {
+      return "content-container-active";
+    } else {
+      return "content-container-hidden";
+    }
+  }
+
+  useEffect(() => {
+    if (totalContents <= 1) return;
+
+    const interval = setInterval(() => {
+      nextContent();
+
+      setCounter((prev) => prev + 1);
+    }, 3000);
+
+    return () => clearInterval(interval);
+
+  }, [content, totalContents])
+
   return (
-    <div>
+    <div className="content-carousel">
       {schools.map((item, idx) => {
         return (
-        <div key={idx}>
-          <h1>{item.name}</h1>
-          <h2>{item.course}</h2>
-          <h2>{item.duration}</h2>
-          <table>
-
-          </table>
-        </div>
+          <div className={getContainerVisibility(getVisibility(idx))} key={idx}>
+            <div className={getVisibility(idx)}>
+              <h1>{item.name}</h1>
+              <h2>{item.course}</h2>
+              <h2>{item.duration}</h2>
+            </div>
+          </div>
         );
 
       })}
