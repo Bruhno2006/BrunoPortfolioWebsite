db4f585790444fe60cb0b7b368f99da88b26cd82
Bruhno2006 <brunoamadi8@gmail.com>
2026-07-28 22:43:16 +0100

    #19 #25 #26 Further Developed the Education Section

--- bruno/src/components/EducationTable.jsx
+++ bruno/src/components/EducationTable.jsx
@@ -3,9 +3,7 @@ import { schools } from "../data/educationData.json";
 import "./style/EducationTable.css"
 
 function EducationTable() {
-  const [content, setContent] = useState(0);
-  const [counter, setCounter] = useState(0);
-
+  const [contentIndex, setContentIndex] = useState(0);
   const totalContents = schools.length;
 
   const nextContent = () => {
@@ -13,7 +11,7 @@ function EducationTable() {
   }
 
   const getVisibility = (idx) => {
-    if (content === idx) {
+    if (contentIndex === idx) {
       return "content-active"
     }
 
@@ -32,37 +30,85 @@ function EducationTable() {
     if (totalContents <= 1) return;
 
     const interval = setInterval(() => {
-      nextContent();
-
-      setCounter((prev) => prev + 1);
+      setContentIndex((prev) => (prev === totalContents - 1 ? 0 : prev + 1));
     }, 3000);
 
     return () => clearInterval(interval);
+  }, [totalContents]);
 
-  }, [content, totalContents])
+  useEffect(() => {
+    if (!schools || schools.length === 0) {
+      return;
+    }
+
+    const prevIndex = contentIndex === 0 ? totalContents - 1 : contentIndex - 1;
+    const previousColour = schools[prevIndex].main_colour;
+    const currentColour = schools[contentIndex].main_colour;
+
+    setPreviousBackgroundColour(previousColour);
+    setCurrentBackgroundColour(currentColour);
+  }, [contentIndex, totalContents]);
+
+
+  const setCurrentBackgroundColour = (colour) => {
+    var root = document.documentElement;
+
+    root.style.setProperty('--current-background-colour', colour);
+  }
+
+  const setPreviousBackgroundColour = (colour) => {
+    var root = document.documentElement;
+
+    root.style.setProperty('--previous-background-colour', colour);
+  }
 
   return (
-    <div className="content-carousel">
-      {schools.map((item, idx) => {
-        return (
-          <div className={getContainerVisibility(getVisibility(idx))} key={idx}>
-            <div className={getVisibility(idx)}>
-              <h1>{item.name}</h1>
-              <h2>{item.course}</h2>
-              <h2>{item.duration}</h2>
-              <div className="results-overview">
-                {item.results.map((result, idx) => {
-                  return (
-                    <div key={idx}>
-                      <p>Year {idx + 1}: {result}</p>
-                    </div>
-                  );
-                })}
+    <div>
+      <div className="content-carousel">
+        {schools.map((item, idx) => {
+          return (
+            <div className={getContainerVisibility(getVisibility(idx))} key={idx}>
+              <div className={getVisibility(idx)}>
+                <div>
+                  <img className="school-logo" src={item.logo} alt={item.alt} />
+                </div>
+                <div className="education-information">
+                  <h1>{item.name}</h1>
+                  <h2>{item.course}</h2>
+                  <h2>{item.duration}</h2>
+                  <div className="results-overview">
+                    {item.results.map((result, idx) => {
+                      return (
+                        <div key={idx}>
+                          <p>Year {idx + 1}: {result}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
+      <div className="short-overview-carousel">
+        {schools.map((item, idx) => {
+          return (
+            <div className={getContainerVisibility(getVisibility(idx))} key={idx}>
+              <div className={getVisibility(idx)}>
+                <div>
+                  <img className="school-logo" src={item.logo} alt={item.alt} />
+                </div>
+                <div className="education-information">
+                  <h1>{item.name}</h1>
+                  <h2>{item.course}</h2>
+                  <h2>{item.duration}</h2>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
