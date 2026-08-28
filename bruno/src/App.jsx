78bee5d7b1ca7378a99142a85f2fd05d2149ff8d
Bruhno2006 <brunoamadi8@gmail.com>
2026-08-28 23:17:12 +0100

    #37 #38 Combined About Us and Introduction Sections, beginning later improvements

--- bruno/src/App.jsx
+++ bruno/src/App.jsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from 'react';
-import { GitBranchIcon, Heart, Link, Mail, Phone } from 'lucide-react';
 import './style/App.css';
 import Loader from './components/Loader.jsx';
 import ProjectSlider from './components/ProjectSlider.jsx';
-import { slides } from "./data/projectData.json";
 import AppNavbar from './components/AppNavbar.jsx';
 import EducationTable from './components/EducationTable.jsx';
-import { SiGithub, SiInstagram } from '@icons-pack/react-simple-icons';
 import AppearanceToggle from './components/AppearanceToggle.jsx';
+import { slides } from "./data/projectData.json";
+import AppFooter from './components/AppFooter.jsx';
 
 function App() {
   const date = new Date().getFullYear();
@@ -50,81 +49,40 @@ function App() {
     return (
       <Loader />
     );
-  } else {
-    return (
-      <>
-        <AppNavbar />
-        <AppearanceToggle />
-        <div className="main-content">
-          <section className="image-with-text" id='introduction'>
-            <div className="introduction-photo">
-              <img src="https://picsum.photos/700/500?random=1" />
-            </div>
-            <div className="introduction-content">
-              <h2 className="introduction-header">Introduction</h2>
-              <p className="introduction-description">
-                My name is Bruno Amadi. I am a {date - 2006} year old who's striving on becoming a great software engineer that is able to create websites, software, and 
-                programs.<br/><br/>
-                Originally a side-project, I have made my website not only as a portfolio, but to document my progress in my software engineering journey and list all my past and
-                upcoming projects. These projects range from functional software, viewable websites, playable games, and external fun side-projects and hobbies.<br/><br/> 
-                
-                It would be like a large <strong>Bruno</strong> archive!!!<br/><br/>
-                
-                <strong><em>SO AWESOME!</em></strong></p>
-            </div>
-          </section>
-          <section className="image-with-text-reverse" id='about-me'>
-            <div className="about-me-photo">
-              <img src="https://picsum.photos/700/500?random=2" />
-            </div>
-            <div className="about-me-content">
-              <h2 className="about-me-header">About Me</h2>
-              <p className="about-me-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-          </section>
-          <section id="education">
-            <div className="education-content">
-              <h2 className="education-header">Education</h2>
-              <EducationTable />
-            </div>
-          </section>
-          <section id="projects-and-achievements">
-            <div className="projects-content">
-              <h2 className="projects-header">Projects and Achievements</h2>
-              <ProjectSlider data={slides} />
-            </div>
-          </section>
-          <section className="text-in-center" id="contacts">
-            <div className="contacts-content">
-              <h2 className="contacts-header">Contact Information</h2>
-              <p className="contacts-description">Feeling interested? Either contact me or look into my other socials!</p>
-              <li className="contact-list">
-                {[
-                  { icon: <Mail size={30} color="var(--link-colour)" />, text: "brunoamadibusiness@gmail.com", link: "mailto:brunoamadibusiness@gmail.com" },
-                  { icon: <Phone size={30} color="var(--link-colour)" />, text: "(+44) 7400199297", link: "tel:+447400199297" },
-                  { icon: <SiGithub size={30} color="var(--link-colour)" />, text: "GitHub", link: "https://github.com/Bruhno2006" },
-                  { icon: <SiInstagram size={30} color="var(--link-colour)" />, text: "Instagram", link: "https://www.instagram.com/brunkus7767/" },
-                  { icon: <Link size={30} color="var(--link-colour)" />, text: "LinkedIn", link: "https://www.linkedin.com/in/bruno-amadi/" },
-                ].map(({ icon, text, link }, i) => {
-                  return (
-                    <a key={i} href={link} target='_blank' className="contact-link">
-                      <ul>
-                        <span className="contact-link-icon">{icon}</span>
-                        <span className="contact-link-name">{text}</span>
-                      </ul>
-                    </a>
-                  );
-                })}
-              </li>
-            </div>
-          </section>
-        </div>
-      </>
-    )
   }
+
+  return (
+    <>
+      <AppNavbar />
+      <AppearanceToggle />
+      <div className="main-content">
+        <section className="image-with-text" id="about-me">
+          <div>
+            {/* <img src="https://picsum.photos/1700/200?random=1" /> */}
+          </div>
+          <p className="description">
+            My name is Bruno Amadi. I am a {date - 2006} year old who's striving on becoming a great software engineer that is able to create websites, software, and
+            programs.<br /><br />
+            Originally a side-project, I have made my website not only as a portfolio, but to document my progress in my software engineering journey and list all my past and
+            upcoming projects. These projects range from functional software, viewable websites, playable games, and external fun side-projects and hobbies.<br /><br />
+
+            It would be like a large <strong>Bruno</strong> archive!!!<br /><br />
+
+            <strong><em>SO AWESOME!</em></strong>
+          </p>
+        </section>
+        <section id="education">
+          <EducationTable />
+        </section>
+        <section id="projects-and-achievements">
+          <ProjectSlider data={slides} />
+        </section>
+        <section id="footer">
+          <AppFooter />
+        </section>
+      </div>
+    </>
+  );
 }
 
 export default App;
