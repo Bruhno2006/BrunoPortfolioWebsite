4b2d9888bf0d2383077cd2e36a72be9b0fe60d0d
Bruhno2006 <brunoamadi8@gmail.com>
2026-09-14 21:44:17 +0100

    #42 Implemented Pagination functionality, need styling

--- bruno/src/BlogHome.jsx
+++ bruno/src/BlogHome.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import posts from './data/postsData.js';
 import AppNavbar from './components/AppNavbar';
@@ -6,18 +6,27 @@ import AppearanceToggle from './components/AppearanceToggle';
 import AppFooter from './components/AppFooter.jsx';
 import './style/BlogHome.css'
 import BlogCard from './BlogCard.jsx';
+import Pagination from './components/Pagination.jsx'
 
 const BlogHome = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(4);
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
   return (
     <>
       <AppNavbar />
       <AppearanceToggle />
       <main className="blog-main">
         <div className="grid-container">
-          {posts.map((post) => (
+          {currentPosts.map((post) => (
             <BlogCard key={post.id} {...post} />
           ))}
         </div>
+        <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} setCurrentPage={setCurrentPage} currentPage={currentPage} />
       </main>
       <AppFooter />
     </>
